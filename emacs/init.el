227b561bf366e6c3dcf37e457afa7882a8ae6b05
LucasNomi <lucas04.dev@gmail.com>
2023-07-01 20:59:09 -0300

    feat: auto-package-update installed

--- emacs/init.el
+++ emacs/init.el
@@ -49,6 +49,18 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+(use-package auto-package-update
+  :ensure t
+  :custom
+  (auto-package-update-interval 60)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "23:00"))
+  
+  
+
 (use-package diminish
   :ensure t)
 
