f17cef79b6fbfb7293b65b57ab8a265152370eef
LucasNomi <lucas04.dev@gmail.com>
2023-08-06 12:23:03 -0300

    feat: install script

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
- 
+#!/bin/bash
+
+programs=("neofetch" "emacs" "vim" "git" "tmux" "alacritty")
+
+for str in ${programs[@]}; do
+    if ! [ -x "$(command -v $str)" ]; then
+        echo "$str not found."
+        echo "Proceeding to installation..."
+        sudo apt install $str 2>/dev/null
+    else
+        echo "$str found"
+    fi
+done
+
+
